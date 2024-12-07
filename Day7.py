b7ce3db9838647dbd2724c3e4eeb9cfea40803bc
einspieler <alexis66@gmx.at>
2024-12-07 17:36:27 +0100

    Another optimization to Day 7

--- Day7.py
+++ Day7.py
@@ -1,25 +1,25 @@
 with open('Input/Day7.txt', 'r') as inputfile:
     input = inputfile.readlines()
 
-def is_addable(result, interim, numlist, i):
+def is_addable(result, interim, numlist):
     if interim > result:
         return False
-    if i == len(numlist):
+    if not numlist:
         return result == interim
-    return is_addable(result, interim + numlist[i], numlist, i + 1) or is_addable(result, interim * numlist[i], numlist, i + 1)
+    return is_addable(result, interim + numlist[0], numlist[1:]) or is_addable(result, interim * numlist[0], numlist[1:])
     
-def is_addable_part2(result, interim, numlist, i):
+def is_addable_part2(result, interim, numlist):
     if interim > result:
         return False
-    if i == len(numlist):
+    if not numlist:
         return result == interim
-    return is_addable_part2(result, interim + numlist[i], numlist, i + 1) or is_addable_part2(result, interim * numlist[i], numlist, i + 1) or is_addable_part2(result, int(f'{interim}{numlist[i]}'), numlist, i + 1)
+    return is_addable_part2(result, interim + numlist[0], numlist[1:]) or is_addable_part2(result, interim * numlist[0], numlist[1:]) or is_addable_part2(result, int(f'{interim}{numlist[0]}'), numlist[1:])
 
 resultlist = [int(line.split(':')[0]) for line in input]
 numlist = [[int(x) for x in sublist] for sublist in [line.replace('\n', '').split(':')[1].split(' ')[1:] for line in input]]
 
-result = sum(resultlist[i] for i in range(len(resultlist)) if is_addable(resultlist[i], numlist[i][0], numlist[i], 1))
+result = sum(resultlist[i] for i in range(len(resultlist)) if is_addable(resultlist[i], numlist[i][0], numlist[i][1:]))
 print(result)
 
-result_concat = sum(resultlist[i] for i in range(len(resultlist)) if is_addable_part2(resultlist[i], numlist[i][0], numlist[i], 1))
+result_concat = sum(resultlist[i] for i in range(len(resultlist)) if is_addable_part2(resultlist[i], numlist[i][0], numlist[i][1:]))
 print(result_concat)
