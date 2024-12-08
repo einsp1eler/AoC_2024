8ed649e724c7e280ee84b01b0ea200dc91287ffe
einspieler <alexis66@gmx.at>
2024-12-08 02:43:52 +0100

    Small Julia adjustment

--- Day7.jl
+++ Day7.jl
@@ -22,7 +22,7 @@ input = readlines("Input/Day7.txt")
 
 resultlist = [parse(Int, split(line, ":")[1]) for line in input]
 
-numlist = [[parse(Int, x) for x in split(split(replace(line, "\n" => ""), ":")[2])] for line in input]
+numlist = [[parse(Int, x) for x in split(split(line, ":")[2])] for line in input]
 
 result = sum(resultlist[i] for i in 1:length(resultlist) if is_addable(resultlist[i], numlist[i][1], numlist[i][2:end]))
 println(result)
